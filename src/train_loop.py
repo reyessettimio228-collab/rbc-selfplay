2bc790c15a01ff33b6a2d6eba865f194628b3068
reyessettimio228-collab <reyessettimio228@gmail.com>
2026-02-16 15:11:10 +0100

    Update train_loop.py

--- src/train_loop.py
+++ src/train_loop.py
@@ -1,1 +1,17 @@
+from typing import Any, Dict, List
+
+from src.config import Config
+from src.utils import set_seed
+
+
+def train_from_data(cfg: Config, data: List[Dict[str, Any]]) -> Any:
+    """
+    Train your policy/value network from self-play data.
+    Return the trained model (or a dict of weights).
+    """
+    set_seed(cfg.seed)
+
+    model: Any = None
+    # TODO: move training loop from notebook here
+    return model
 
